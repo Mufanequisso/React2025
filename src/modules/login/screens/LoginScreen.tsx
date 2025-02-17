bb7a0f47cf9224fb781104a08c9506ab299cc58c
Paulino Mufanequisso <paulinomufanequisso@gmail.com>
2025-02-16 17:10:45 -0800

    criacao do modulo login

---
+++ src/modules/login/screens/LoginScreen.tsx
@@ -0,0 +1,11 @@
+import { BackgroundImage } from "../styles/LoginScreen.styles";
+
+const LoginScreen = () =>{
+
+    return  <div>
+        <BackgroundImage src="./Background3.jpg"/>
+
+    </div>
+
+};
+export default LoginScreen;
